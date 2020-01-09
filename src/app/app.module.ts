2e589220a692f8657df08c2753319db82376ceeb
EDGZTNSR <zombiecolony9@gmail.com>
2020-01-09 11:17:41 +0100

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,28 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
-  ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent
-  ],
-  bootstrap: [ AppComponent ]
+   imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot([
+        { path: '', component: ProductListComponent },
+        { path: 'products/:productId', component: ProductDetailsComponent },
+        { path: 'cart', component: CartComponent },
+      ])
+   ],
+   declarations: [
+      AppComponent,
+      TopBarComponent,
+      ProductListComponent,
+      ProductDetailsComponent,
+      CartComponent
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
