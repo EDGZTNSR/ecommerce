85dd6b1a71d13804dbd0061ab982e8f9fd449f41
EDGZTNSR <zombiecolony9@gmail.com>
2020-01-09 11:18:07 +0100

    modified data source

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,26 +4,26 @@ export const products = [
     price: 420,
     description: 'A newly developed deepfried meme',
     creator: 'AsBadAsJava',
-    source: 'CEASETHISACTIVITIES.jpg'
+    source: 'CEASETHISACTIVITIES.jpg',
+    specialPrice: 1.0,
+    normalPrice: 1.5
   },
   {
     name: 'TIANANMEN SQUARE',
     price: 699,
     description: 'Didn\'t happen',
     creator: 'Chinese Government',
-    source: 'TIANANMEN.jpg'
+    source: 'TIANANMEN.jpg',
+    specialPrice: 19.0,
+    normalPrice: 10.5
   },
   {
     name: 'RARE PEPE',
     price: 299,
     description: 'HATE ME',
-    source: '/img/TIANANMEN.jpg'
+    source: '/img/TIANANMEN.jpg',
+    specialPrice: 2.0,
+    normalPrice: 5.0
   }
+  
 ];
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
